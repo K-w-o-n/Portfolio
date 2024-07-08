4c6c9f6dd0bc6a7661b50d4307df3dffa300aa75
K-w-o-n <kwonfsde95@gmail.com>
2024-07-08 09:49:27 +0630

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero.jsx'
-import Bot from './Components/Bot.jsx'
+// import Bot from './Components/Bot.jsx'
 import About from './Components/About.jsx'
 import Skill from './Components/Skill.jsx'
 import Project from './Components/Project.jsx'
@@ -15,7 +15,6 @@ function App() {
     <div className="App">
       <Navbar/>
       <Hero/>
-      <Bot/>
       <About/>
       <Skill/>
       <Project/>
